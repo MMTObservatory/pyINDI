11a8ad0146ae7bc34d576553060b005c15242d7f
Scott Swindell <srswinde@gmail.com>
2020-09-30 10:25:49 -0700

    Better seperation between indiclient and webclient

--- example_clients/webapp.py
+++ example_clients/webapp.py
@@ -6,20 +6,40 @@ import sys
 import logging
 import click
 import tornado
+import datetime
 logging.basicConfig(level=logging.DEBUG)
 sys.path.insert(0, str(Path.cwd().parent))
+import tornado.web
 
-
-from pyindi.webclient import INDIWebApp, IndiHandler
+from pyindi.webclient import INDIWebApp, IndiHandler, INDIWebClient
 
 class Apogee(IndiHandler):
 
     def get(self):
+        
+        self.indi_render(Path.cwd()/"apogee.html", device_name="Apogee CCD")
 
-        self.render("./apogee.html", device_name="Apogee CCD")
 
 
-wa = INDIWebApp(webport=5905)
+def handle_blob(blob):
+    now = datetime.datetime.now()
+    tstr = now.strftime("%d%m%y-%H%M%S")
+    fname = f"imgs/{blob['name']}_{tstr}{blob['format']}"
+    logging.debug(f"saving blob to {fname}")
 
-wa.build_app([(r"/apogee.html", Apogee)], debug=True)
+    with open(fname, 'wb') as fd:
+        fd.write(blob['data'])
+
+        
+
+
+wa = INDIWebApp(webport=5905, handle_blob=handle_blob)
+imgs = Path('./imgs')
+imgs.mkdir(exist_ok=True)
+
+wa.build_app(
+    [(r"/", Apogee), 
+     (r"/imgs/(.*)", tornado.web.StaticFileHandler, {"path":imgs})
+    ], 
+    debug=True)
 
--- pyindi/client.py
+++ pyindi/client.py
@@ -1,4 +1,3 @@
-import tornado.web, tornado.ioloop, tornado.websocket
 import time
 import asyncio
 from lxml import etree
@@ -8,120 +7,85 @@ import json
 from pathlib import Path
 import logging
 
-from xml.sax import ContentHandler
-from xml.sax.expatreader import ExpatParser
 
-from io import StringIO, BytesIO
-
-from base64 import b64decode
 
 """
-    This module contains classes and methods to build an INDI
-    client webapp. The data flow in this program is a bit 
-    confusing. This is made worse because of the somewhat 
-    ambiguous terms of server and client. Hopefully this flowchart
-    will help:
-    
+    INDIClient runs two tasks that are infinite loops and run 
+    in parallel with the asyncio.gather function. These
+    tasks, read_from_indiserver and write_to_indiserver, are 
+    explained in the class diagram below. 
 
-        Overall INDI server/client to webpage scheme
+        Overall INDI server/client scheme
         --------------------------------------------
        
-              _____________________________________________________________________       
-              | client.py                                                         |
-              | ---------                     ___________                         |
-              |                 _____get()____|web2indiQ|____put()____________    |
-              |                 |             -----------                    |    |
-              |                 |                                            |    |
-____________  |           ______V_____                      httpclients      |    |
-|          | <|--writer---|          |                     _______________   |    |
-|indiserver|  |  TCP/IP   |INDIClient|                     |---->client_1|   |    |  
-|          | -|--reader-->|          |---indiclient2web()->|---->client_2|   |    | __________
-------------  |           ------------               |     |---->...     |<-websoc->|webpages|
-              |                 ^                    |     |---->client_n|        | ----------
-              |                 |                    |     ---------------        |
-              |                 |                    V                            |
-              |         _____________            __________________               |
-              |         |BlobHandler|<--feed()---|ServerSideClient|               |
-              |         -------------            ------------------               |
-              ---------------------------------------------------------------------    
+              ___________________________________________________________       
+              | INDIClient                                              |   
+              | ----------                                              |
+ ____________ |                                                         |
+ |          | |             ________________________             _______________________
+ |          |---->reader--->|read_from_indiserver()|------------>|xml_from_indiserver()|
+ |indiserver| |             ------------------------             -----------------------
+ |          | |                                                         |
+ |          | |             _______________________              _______|_______________
+ |          |<-----writer---|write_to_indiserver()|<--to_indiQ<--|xml_to_indiserver()  |
+ ------------ |             -----------------------              -----------------------
+              |                                                         |
+              -----------------------------------------------------------
 
+    To build an INDIClient application sublcass INDIClient and override
+    the xml_from_indiserver to retrieve data from the indiserver and
+    use the xml_to_indiserver method to send data to the indiserver. An
+    example of this is in webclient.py in this package.  
 
-   
+    TODO:
+        it would be nice to remove tornado dependencies in this module. 
 
 """
 
-
-
-class WebHandler(tornado.web.RequestHandler):
-    """
-    Render a simple webpage. 
-    The page uses setPropertyCallback
-    to render the device given in the url eg:
-    If the url string is /dev/(.*) and the 
-    url entered into the browser is /dev/mount
-    the webpage will send a setPropertyCallback('mount.*')
-    and build all the non-BLOB properties into jquery-ui
-    widgets. 
-    """
-
-    static_path = Path(__file__).parent/"www/index.html"
-    
-    def get(self, device):
-        self.render(str(self.static_path), device_name=device)
-
-
-
 class INDIClient:
-    """This sends/recvs INDI data to/from the indiserver 
-    tcp/ip socket. """
+    """This class sends/recvs INDI data to/from the indiserver 
+    tcp/ip socket. See the above diagram for help understanding
+    its data flow.  """
 
-    _instance = None
-    blob = None
-    blobmeta = None
-
-    def __new__(cls, *args, **kwargs):
-        """
-        Make it a singleton
-        """
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
+    to_indiQ = Queue()
 
 
-    web2indiQ = Queue()
-    httpclients = set()
-    once = True
-
-    def start(self, host="localhost", port=7624):
+    def start(self, host="localhost", port=7624, read_width=30000):
         self.port = port
         self.host = host
-        self.httpclients.add(ServerSideClient())
+        self.read_width=read_width
         self.lastblob = None
 
-    async def indiclient2web(self):
 
-        """Read data from self.reader and use write_message
-        to write that data to each client."""
+    async def xml_from_indiserver(self, data):
 
-        self.running = 1
+        raise NotImplemented("This method should be implemented by the subclass")
+
+
+    async def read_from_indiserver(self):
+
+        """Read data from self.reader and then call
+        xml_from_indiserver with this data as an arg."""
+
         while self.running:
             try:
                 if self.reader.at_eof():
                     raise Exception("INDI server closed")
 
                 # Read data from indiserver
-                data = await self.reader.read(1000)
-                for client in self.get_httpclients():
-                    client.write_message(data)
+                data = await self.reader.read(self.read_width)
+                await self.xml_from_indiserver(data)
+
+
             except Exception as err:
                 self.running = 0
                 logging.warning(f"Could not read from INDI server {err}")
+                raise
 
         self.writer.close()
         await self.writer.wait_closed()
-        logging.warning(f"Finishing indiclient2web task")
+        logging.warning(f"Finishing read_from_indiserver task")
         
-
 
     async def connect(self):
         """Attempt to connect to the indiserver in a loop.
@@ -135,8 +99,8 @@ class INDIClient:
                 logging.debug(f"Connected to indiserver {self.host}:{self.port}")
                 self.running = True
 
-                self.task = asyncio.gather(self.indiclient2web(),
-                        self.web2indiserver(), return_exceptions=True)
+                self.task = asyncio.gather(self.read_from_indiserver(),
+                        self.write_to_indiserver() )
                 await self.task
                 logging.debug("INDI client tasks finished. indiserver crash?")
                 logging.debug("Attempting to connect again")
@@ -154,286 +118,98 @@ class INDIClient:
 
             await asyncio.sleep(2.0)
 
+    
+    async def xml_to_indiserver(self, xml):
+        """
+        put the xml argument in the 
+        to_indiQ. 
+        """
+        await self.to_indiQ.put(xml)
+        
+        
 
-
-    async def web2indiserver(self):
-        """Collect INDI data from the httpclient via the web2indiQ.
+    async def write_to_indiserver(self):
+        """Collect INDI data from the from the to_indiQ.
+        and send it on its way to the indiserver. 
         """
 
         while self.running:
             try:
-                fromweb = await asyncio.wait_for( self.web2indiQ.get(), 5 )
+                to_indi = await asyncio.wait_for( self.to_indiQ.get(), 10 )
+                logging.debug(f"writing this to indi {to_indi}")
             except asyncio.TimeoutError as error:
-
                 # This allows us to check the self.running state
-                # if there is no data in the web2indiQ
+                # if there is no data in the to_indiQ
                 continue
             try:
-                self.writer.write(fromweb.encode())
+                self.writer.write(to_indi.encode())
                 await self.writer.drain()
             except Exception as err:
                 self.running = 0
                 logging.debug(f"Could not write to INDI server {err}")
 
-        logging.debug("Finishing web2indiserver task")
+        logging.debug("Finishing write_to_indiserver task")
 
 
     
-    def put_blob(self, blob, **meta):
-        self.lastblob = {}
-        self.lastblob.update(meta)
-        self.lastblob['data'] = blob.read()
-        
-        
-    
-    def get_blob(self):
-        return self.lastblob
+class INDIClientSingleton(INDIClient):
+    """
+    INDIClient as a singleton. This works well
+    if you only want one client and you want 
+    to be able to easily access that client
+    by simple instantiation. 
+    """
 
-    # These methods are how we keep up with 
-    # The clients. I don't think they need 
-    # to be class methods. 
-    @classmethod
-    def get_httpclients(cls):
-        return cls.httpclients
+    _instance = None
 
-    @classmethod
-    def add_httpclient(cls, client):
-        cls.httpclients.add(client)
-
-
-    @classmethod
-    def remove_client(cls, client):
-        cls.httpclients.remove(client)
-
-
-
-class BlobHandler(ContentHandler):
-
-    blobnames = set()
-    reading = False
-
-    def __init__(self):
-        self.indiclient = INDIClient()
-
-        super().__init__()
-
-    def startElement(self, tag, attr):
-
-        if tag == "defBLOBVector":
-            self.blobnames.add(attr["name"])
-
-        if tag == "oneBLOB":
-            try:
-                logging.debug(f"we have a blob! {tag} {dict(attr)}")
-                
-                self.reading = True
-                self.current_blob = StringIO()
-                self.attr = dict(attr)
-                self.len = 0
-
-            except Exception as error:
-                logging.warning("Error with start of BLOB element {error}")
-
-
-    def characters(self, content):
-        """Read handle character data from the
-        xml feed. For the BLOB's this data is 
-        base64 encoded. We write this to an 
-        in memory file for later processing."""
-        if self.reading:
-
-            self.current_blob.write(content.strip())
-
-            #if len(self.current_blob) > int(self.attr["enclen"]):
-                #raise RuntimeError("Too much BLOB data!")
-                
-
-    
-    def endElement(self, tag):
+    def __new__(cls, *args, **kwargs):
         """
-            Called when the end tag is reached
-            This is where we finish processing
-            the BLOB.
+        Make it a singleton
         """
-        
-        if "blob" in tag.lower():
-            logging.debug(f"end tag {tag}")
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
 
-        if tag == "oneBLOB":
-            try:
-                logging.debug(f"BLOB finished ")
-                self.reading = False
-                self.current_blob.seek(0)
-                
-                # Convert to raw binary. 
-                bindata = BytesIO(b64decode(self.current_blob.read()))
-                
-                self.indiclient.put_blob(bindata, **self.attr)
 
-            except Exception as error:
-                logging.warning(f"Error putting blob {error}")
-                
-            
-            
-            
+
+class INDIClientContainer:
+
+    def __new__(cls, *args, **kwargs):
+        """
+        Make it a singleton
+        """
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance._clients = []
+
+        return cls._instance
             
 
-class ServerSideClient:
-
-    """Sometimes it is necessary to understand the 
-    incoming INDI information on the serverside as 
-    opposed to sending the raw information to the
-    httpclient. 
-    
-    
-    Here we would like to extract BLOB data so that
-    it can be saved to disk. To do this we feed the
-    xml to the ExpatParser which uses BlobHandler to
-    pull out the base64 data and convert it to binary.
-    """
-
-    def __init__(self):
-        self.parser = ExpatParser()
-        self.parser.setContentHandler( BlobHandler() )
-
-        # we need to fake a root element
-        self.parser.feed("<root>")
-
-    def write_message(self, data):
-        self.parser.feed(data)
-
-
-class INDIWebsocket(tornado.websocket.WebSocketHandler):
-    """
-    This class handles the websocket traffic and 
-    registers new http clients with the INDIClient class
-    """
-    client = INDIClient
-    
-    def open(self):
-        self.client.add_httpclient(self)
-
-
-    def on_message(self, message):
-        self.client.web2indiQ.put(message)
-
-    def on_close(self):
-        self.client.httpclients.remove(self)
-
-
-class INDIWebApp():
-    """
-        This class takes the indiclient
-        and builds it into a tornado webapp
+    def create_client(self, *args, **kwargs):
         
-    """
-    # The files in this path will alway be available
-    # http://<HOST>/static/
-    static_path = Path(__file__).parent/"www/static"
+        if "client_class" in kwargs:
+            if issubclass(kwargs["client_class"], INDIClient):
+                client_class = kwargs["client_class"]
+                del kwargs["client_class"]
+            else:
+                raise TypeError(f"client_class must be subclass of INDIClient.")
+        else:
+            client_class = INDIClient
 
-    def __init__(self, loop=None, webport=8888, indihost="localhost",
-            indiport=7624):
-        """
-        Arguments:
-        loop: The tornado IOLoop can not be an asyncio event loop.
-        webport: The port of the webserver. 
-        indihost: the name or IP address of the computer hosting the indi
-        server.
-        indiport: The port the indi server is running on. 
-        """
-
-        self._handlers = None
-        self.port = webport
+        client = client_class()
+        client.start(*args, **kwargs)
         
+        self._clients.append(client)
+
+        return client
+
+    def new_client(self, client=INDIClient):
+
+        self._clients.append(client)
+     
+   
+    def __getitem__(self, key):
+
+        return self._clients[key]
         
-
-        if loop is None:
-            loop = tornado.ioloop.IOLoop.current()
-
-        self.mainloop = loop
-
-        self.client = INDIClient(port=indiport, host=indihost)
-
-        self.mainloop.add_callback(self.client.connect)
-
-
-    def add_page(self, url_path: str, path_to_html: str):
-        """
-        Add a webpage to be rendered by tornado.
-        Get data only. 
-        """
-        html = Path(path_to_html)
-        if self._handlers is None:
-            self._handlers = []
-
-        class handler(tornado.web.RequestHandler):
-
-            def get(self):
-
-                name = self.get_argument("device_name", None)
-
-                # This bit makes a requirement that the 
-                # webpage include a device_name argument
-                # perhaps this is a bit draconian. 
-                if name is None:
-                    self.write(f"URL must contain device_name as http get data. For\
-                            example: <br><br><br> http://{self.request.host}/dev?device_name=mydev\
-                            ")
-
-                # Convenience vars for the template
-                indihead = r"""
-                    <script src="//code.jquery.com/jquery-1.12.4.js"></script>
-                    <script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-
-                    <!--These libraries are built with pyINDI
-                    and are available at /static/ using
-                    pyINDI's client libarary.-->
-                
-                    <link rel="stylesheet" href="/static/indi/indi.css">
-                    <script src="/static/indi/indi.js"></script>
-                    <script src="/static/indi/utility.js"></script>
-                    <script src="/static/indi/maps-indi.js"></script>
-
-                    """
-                        
-                indisocket = f"http://{self.request.host}/indi-websocket"
-
-                return self.render( 
-                    str(html), 
-                    device_name=name, 
-                    indihead=indihead, 
-                    indisocket=indisocket)
-
-        self._handlers.append((url_path, handler))
-
-
-    
-        
-    def add_handler(self, handler: tuple):
-        """Expose tornado.web.Application add_handlers method"""
-        if self._handlers is None:
-            self._handlers = []
-        self._handlers.append(handler)
-
-
-
-    def start(self, **settings):
-        if self._handlers is None:
-            # Add the default page
-            self._handlers = [(r"/dev/(.*)", WebHandler)]
-
-        # Add the websocket handler
-        self._handlers.append((r"/indi-websocket", INDIWebsocket))
-        self.app = tornado.web.Application(
-                self._handlers,
-                static_path=self.static_path, 
-                **settings
-                )
-
-        self.app.listen(self.port)
-
-        self.mainloop.start()
-
-
-
 
